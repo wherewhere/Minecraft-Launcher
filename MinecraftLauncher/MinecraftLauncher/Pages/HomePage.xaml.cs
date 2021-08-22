d4fb3f240c9ace16c527033c7d0f8086d5d7320e
wherewhere <qq2518766683@outlook.com>
2021-08-22 14:21:13 +0800

    添加项目文件。

---
+++ MinecraftLauncher/MinecraftLauncher/Pages/HomePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using KMCCC.Tools;
+using Microsoft.UI.Xaml.Controls;
+using ModuleLauncher.Re.Launcher;
+using System;
+using System.Drawing;
+using System.Linq;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MinecraftLauncher.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            Graphics graphics = Graphics.FromHwnd(IntPtr.Zero);
+            float dpiX = graphics.DpiX;
+            float dpiY = graphics.DpiY;
+            GetDPI.Text = $"X:{dpiX} Y:{dpiY}";
+        }
+
+        private async void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            var launcher = new Launcher("C:/Users/qq251/AppData/Roaming/.minecraft")
+            {
+                Java = "C:/Program Files/Java/jre1.8.0_301/bin/javaw.exe",
+                Authentication = "wherewhere",
+                LauncherName = "UWP", //optianal
+                MaximumMemorySize = 1024, //optional
+                MinimumMemorySize = null, //optional
+                WindowHeight = null, //optional
+                WindowWidth = null, //optional
+                Fullscreen = null //optional
+            };
+            StartInfo.Visibility = Microsoft.UI.Xaml.Visibility.Visible;
+            _ = await launcher.Launch("1.16.5");
+            StartInfo.Visibility = Microsoft.UI.Xaml.Visibility.Collapsed;
+        }
+    }
+}
---
+++ MinecraftLauncher/MinecraftLauncher/Pages/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MinecraftLauncher.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationView.SelectedItem = NavigationView.MenuItems[0];
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                _ = NavigationViewFrame.Navigate(typeof(SettingPages.SettingPage), args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.SelectedItemContainer != null)
+            {
+                string NavItemTag = args.SelectedItemContainer.Tag.ToString();
+                switch (NavItemTag)
+                {
+                    case "Home":
+                        _ = NavigationViewFrame.Navigate(typeof(HomePage), args.RecommendedNavigationTransitionInfo);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            try
+            {
+                if (args.SelectedItemContainer.Content != null)
+                {
+                    NavigationView.Header = args.SelectedItemContainer.Content;
+                    NavigationView.PaneTitle = args.SelectedItemContainer.Content.ToString();
+                }
+                else
+                {
+                    NavigationView.Header = " ";
+                    NavigationView.PaneTitle = "麦块启动器";
+                }
+            }
+            catch
+            {
+                NavigationView.Header = " ";
+                NavigationView.PaneTitle = "麦块启动器";
+            }
+        }
+    }
+}
